2a874cbfa6a7c10c9870877a10c4f1266d37a4f2
rochinhaDev <you@example.com>
2023-05-06 11:38:08 -0300

    alterações

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 const homeScreen = document.querySelector("#homeScreen");
 const buttonStart = document.querySelector("#buttonStart");
 const gameScreen = document.querySelector("#gameScreen");
+const board = document.querySelector("#board")
+//variareis
+const deck =[
+    "./images/harmonia.svg",
+    "./images/poder.svg",
+    "./images/projetar.svg",
+    "./images/refletir.svg",
+    "./images/harmonia.svg",
+    "./images/poder.svg",
+    "./images/projetar.svg",
+    "./images/refletir.svg"
+]
 
 buttonStart.addEventListener("click",()=>{
     homeScreen.classList.add("hide")
